792bb3636516ae4dabce761906f5047daa3758fd
sabbir saad <sabbir.saad00@gmail.com>
2021-06-02 23:02:39 +0600

    websited created

---
+++ README.md
@@ -0,0 +1,5 @@
+# Movie Theater
+
+Movie Theater is a online entertainment platform. Where you can watch your favorite movies, series, drama etc.
+
+<a href="https://fire-auth-basic.web.app/">Live</a>
---
+++ src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import movieReducer from "../features/movie/movieSlice";
+import userReducer from "../features/user/userSlice";
+
+export default configureStore({
+    reducer: {
+        user: userReducer,
+        movie: movieReducer,
+    },
+    middleware: getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+});
